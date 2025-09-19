80ac61c5b4b24d1024b117e62f32752f9cb6341a
Amcelo13 <tchetan308@gmail.com>
2025-09-19 14:09:34 -0400

    Explicit wait example

--- Exercise Files/05_03/start/test/Autocomplete.java
+++ Exercise Files/05_03/start/test/Autocomplete.java
@@ -4,9 +4,10 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
 
 public class Autocomplete {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
 
         System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
 
@@ -16,9 +17,15 @@ public class Autocomplete {
 
         WebElement autocomplete = driver.findElement(By.id("autocomplete"));
         autocomplete.sendKeys("1555 Park Blvd, Palo Alto, CA");
-        Thread.sleep(1000);
+        // Thread.sleep(1000);
 
-        WebElement autocompleteResult = driver.findElement(By.className("pac-item"));
+
+        // Explicitly wait
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        
+        WebElement autocompleteResult = wait.until(
+            ExpectedConditions.visibilityOfElementLocated(By.className("pac-item"))
+        );
         autocompleteResult.click();
 
         driver.quit();
