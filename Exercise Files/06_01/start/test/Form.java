0f16deff4a2d26898d0eab2f875157d958815a51
Amcelo13 <tchetan308@gmail.com>
2025-09-19 14:19:02 -0400

    Add date entry and submission functionality to Form.java

--- Exercise Files/06_01/start/test/Form.java
+++ Exercise Files/06_01/start/test/Form.java
@@ -2,15 +2,37 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+import org.openqa.selenium.WebElement; 
 public class Form {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
 
         WebDriver driver = new ChromeDriver();
 
         driver.get("https://formy-project.herokuapp.com/form");
+        WebElement firstName = driver.findElement(By.id("first-name"));
+        firstName.sendKeys("Chetan");
+        WebElement lastName = driver.findElement(By.id("last-name"));
+        lastName.sendKeys("Patel");
+        WebElement jobTitle = driver.findElement(By.id("job-title"));
+        jobTitle.sendKeys("QA Engineer");
+        WebElement education = driver.findElement(By.id("radio-button-2"));
+        education.click();
+
+        WebElement sex = driver.findElement(By.id("checkbox-1"));
+        sex.click();
+
+        WebElement experience = driver.findElement(By.cssSelector("option[value='1']"));
+        experience.click();
+
+        WebElement date = driver.findElement(By.id("datepicker"));
+        date.sendKeys("06/15/2024");
+        date.sendKeys(Keys.RETURN);
+        
+        Thread.sleep(2000); // Just to see the datepicker in action, can be removed
+        WebElement submitButton = driver.findElement(By.cssSelector(".btn.btn-lg.btn-primary"));
+        submitButton.click();
 
         driver.quit();
     }
