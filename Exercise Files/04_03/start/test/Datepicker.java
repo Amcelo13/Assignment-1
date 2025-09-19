1a405afb8e11b32ee0a71a17004a0650e3728343
Amcelo13 <tchetan308@gmail.com>
2025-09-19 13:30:06 -0400

    Update Datepicker.java  and streamline date entry process

--- Exercise Files/04_03/start/test/Datepicker.java
+++ Exercise Files/04_03/start/test/Datepicker.java
@@ -5,13 +5,22 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.By;
 
 public class Datepicker {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
-        System.setProperty("webdriver.chrome.driver", "/Users/meaghanlewis/Downloads/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
 
         WebDriver driver = new ChromeDriver();
 
         driver.get("https://formy-project.herokuapp.com/datepicker");
+
+        WebElement dateField = driver.findElement(By.id("datepicker"));
+        dateField.click();
+
+        // Simulate typing a date
+        dateField.sendKeys("01/01/2026");
+
+        Thread.sleep(1000);  // Pause to see the date entered
+        dateField.sendKeys(Keys.RETURN);   // Close the datepicker? - Ans =
 
         driver.quit();
     }
