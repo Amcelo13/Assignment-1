3facfa18ba3c6ef53e22d35a9046ea766e96ca77
Amcelo13 <tchetan308@gmail.com>
2025-09-19 13:33:17 -0400

    Refactor Dropdown.java to streamline dropdown interaction

--- Exercise Files/04_04/start/test/Dropdown.java
+++ Exercise Files/04_04/start/test/Dropdown.java
@@ -4,13 +4,19 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.By;
 
 public class Dropdown {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
-        System.setProperty("webdriver.chrome.driver", "/Users/meaghanlewis/Downloads/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
 
         WebDriver driver = new ChromeDriver();
 
         driver.get("https://formy-project.herokuapp.com/dropdown");
+        WebElement dropdownButton = driver.findElement(By.id("dropdownMenuButton"));
+        dropdownButton.click();
+
+        Thread.sleep(1000);  // Pause to see the dropdown options
+        WebElement autocompleteItem = driver.findElement(By.id("autocomplete"));
+        autocompleteItem.click();
 
         driver.quit();
     }
